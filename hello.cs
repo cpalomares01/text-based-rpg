0d0000c344c977a063c473dda2e477d07133f235
Cristian Palomares <cp8135482@gmail.com>
2024-03-08 22:42:16 -0800

    Added an irrevlevant but fun comment

--- hello.cs
+++ hello.cs
@@ -1,1 +1,3 @@
 Console.Writeline("Testing out github and its basics");
+//If everything works well, I should be able to just commit then push this.
+//I'm not even sure if i'm using the right language, but i'm having fun! -C.P.
